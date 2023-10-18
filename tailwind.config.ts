daa9c09f646c45a6d9c3aa6d3068cb78fad14502
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-18 13:01:59 -0300

    chore: add next ui & react hook form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,15 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -17,13 +20,14 @@ module.exports = {
     },
     extend: {
       colors: {
+        primaryFox: "#f04600",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#f04600",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
@@ -72,5 +76,29 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [
+    require("tailwindcss-animate"),
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#f04600",
+              foreground: "#000000",
+            },
+            focus: "#f04600",
+          },
+        },
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "#f04600",
+              foreground: "#000000",
+            },
+            focus: "#f04600",
+          },
+        },
+      },
+    }),
+  ],
+};
