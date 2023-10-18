4d38ba78293c5714058a48cb2d4e4baeecf6a390
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-18 13:44:33 -0300

    feat: add next auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import { prismaClient } from "./prisma";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
