0b27d52cefd2a72aed1de3bff2a661c562dfe485
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-19 18:48:06 -0300

    refactor: refactor fetchProducts code

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,85 +4,87 @@ import { prismaClient } from "@/lib/prisma";
 import Image from "next/image";
 
 export default async function Home() {
-  const deals = await prismaClient.product.findMany({
-    where: {
-      discountPercentage: {
-        gt: 0,
+  async function fetchProductsByCategory(categorySlug: string) {
+    return await prismaClient.product.findMany({
+      where: {
+        category: {
+          slug: categorySlug,
+        },
       },
-    },
-    orderBy: {
-      id: "desc",
-    },
-  });
-
-  const headphones = await prismaClient.product.findMany({
-    where: {
-      category: {
-        slug: "headphones",
+      orderBy: {
+        id: "asc",
       },
-    },
-    orderBy: {
-      id: "asc",
-    },
-  });
+    });
+  }
 
-  const mouses = await prismaClient.product.findMany({
-    where: {
-      category: {
-        slug: "mouses",
+  async function fetchProductsByDiscount() {
+    return await prismaClient.product.findMany({
+      where: {
+        discountPercentage: {
+          gt: 0,
+        },
       },
-    },
-    orderBy: {
-      id: "asc",
-    },
-  });
+      orderBy: {
+        id: "desc",
+      },
+    });
+  }
 
-  const headphonesAndMouses = await prismaClient.product.findMany({
-    where: {
-      OR: [
-        { category: { slug: "headphones" } },
-        { category: { slug: "mouses" } },
-      ],
-    },
-    orderBy: {
-      id: "asc",
-    },
-  });
+  async function fetchProductsByCategories(categorySlugs: string[]) {
+    return await prismaClient.product.findMany({
+      where: {
+        category: {
+          slug: {
+            in: categorySlugs,
+          },
+        },
+      },
+      orderBy: {
+        id: "asc",
+      },
+    });
+  }
 
-  const monitorsAndSpeakers = await prismaClient.product.findMany({
-    where: {
-      OR: [
-        { category: { slug: "monitors" } },
-        { category: { slug: "speakers" } },
-      ],
-    },
-    orderBy: {
-      id: "asc",
-    },
-  });
+  const deals = await fetchProductsByDiscount();
+  const headphones = await fetchProductsByCategory("headphones");
+  const mouses = await fetchProductsByCategory("mouses");
+  const monitors = await fetchProductsByCategory("monitors");
+  const speakers = await fetchProductsByCategory("speakers");
+  const headphonesAndMouses = await fetchProductsByCategories([
+    "headphones",
+    "mouses",
+  ]);
+  const monitorsAndSpeakers = await fetchProductsByCategories([
+    "monitors",
+    "speakers",
+  ]);
 
   return (
     <main>
-      <Image
-        src="/banner_home_mobile_01.png"
-        alt="Até 35% de desconto só esse mês"
-        width={0}
-        height={0}
-        className="h-auto w-full md:hidden"
-        sizes="100vw"
-        priority
-      />
+      {/* Desktop Hero Banner - Deals  */}
+      <div>
+        <Image
+          src="/banner_home_mobile_01.png"
+          alt="Até 35% de desconto só esse mês"
+          width={0}
+          height={0}
+          className="h-auto w-full md:hidden"
+          sizes="100vw"
+          priority
+        />
 
-      <Image
-        src="/banner_home_desktop_01.png"
-        alt="Até 35% de desconto só esse mês"
-        width={0}
-        height={0}
-        className="hidden h-96 w-full object-cover md:block"
-        sizes="100vw"
-        priority
-      />
+        <Image
+          src="/banner_home_desktop_01.png"
+          alt="Até 35% de desconto só esse mês"
+          width={0}
+          height={0}
+          className="hidden h-96 w-full object-cover md:block"
+          sizes="100vw"
+          priority
+        />
+      </div>
 
+      {/* Categories list */}
       <section className="mt-5">
         <Categories />
       </section>
@@ -102,7 +104,7 @@ export default async function Home() {
         <ProductList products={deals} />
       </section>
 
-      {/* Desktop 02 banners Headphones + Mouses */}
+      {/* Desktop banner 01 - Headphones + Mouses */}
       <div className="hidden md:block">
         <div className="mx-auto my-5 flex w-full items-center justify-center gap-3 px-5">
           <Image
@@ -127,7 +129,7 @@ export default async function Home() {
         </div>
       </div>
 
-      {/* Desktop product list Headphones + Mouses */}
+      {/* Desktop product list 01 - Headphones + Mouses */}
       <section className="my-5 hidden md:block">
         <p className="mb-4 flex items-center justify-center px-5 text-center text-lg font-medium">
           Potencialize sua jornada digital com os melhores acessórios
@@ -136,7 +138,7 @@ export default async function Home() {
         <ProductList products={headphonesAndMouses} />
       </section>
 
-      {/* Desktop 02 banners Monitors + Speakers */}
+      {/* Desktop banner 02 - Monitors + Speakers */}
       <div className="hidden md:block">
         <div className="mx-auto my-5 flex w-full items-center justify-center gap-3 px-5">
           <Image
@@ -161,7 +163,7 @@ export default async function Home() {
         </div>
       </div>
 
-      {/* Desktop product list Monitors + Speakers */}
+      {/* Desktop product list 02 - Monitors + Speakers */}
       <section className="my-5 hidden md:block">
         <p className="mb-4 flex items-center justify-center px-5 text-center text-lg font-medium">
           Descubra a combinação perfeita de qualidade de som e imagem em nossa
@@ -171,22 +173,22 @@ export default async function Home() {
         <ProductList products={monitorsAndSpeakers} />
       </section>
 
-      {/* Desktop 02 banners Headphones + Mouses */}
+      {/* Desktop Banner 03 -  Delivery */}
       <div className="hidden md:block">
-        <div className="mx-auto my-5 flex w-full items-center justify-center gap-3 px-5">
+        <div className="mt-5 w-full">
           <Image
             src="/banner-fretegratis.png"
             alt="Até 15% de desconto em mouses"
             width={0}
             height={0}
-            className="h-auto w-full rounded-2xl"
+            className="h-auto w-full"
             sizes="100vw"
             priority
           />
         </div>
       </div>
 
-      {/* Mobile 01 banner - Headphones */}
+      {/* Mobile banner 01 - Headphones */}
       <div className="md:hidden">
         <div className="my-5 flex w-full gap-2">
           <Image
@@ -201,17 +203,17 @@ export default async function Home() {
         </div>
       </div>
 
-      {/* Mobile product list Headphones*/}
+      {/* Mobile product list 01 - Headphones*/}
       <section className="my-5 md:hidden">
         <h2 className="mb-2 pl-5 text-lg font-semibold">Headphones</h2>
         <ProductList products={headphones} />
       </section>
 
-      {/* Mobile 02 banner - Mouses */}
+      {/* Mobile banner 02 - Mouses */}
       <div className="md:hidden">
         <div className="my-5 flex w-full gap-2">
           <Image
-            src="/mouses_mobile.png"
+            src="/banner-mouses.png"
             alt="Até 15% de desconto em mouses"
             width={0}
             height={0}
@@ -222,11 +224,68 @@ export default async function Home() {
         </div>
       </div>
 
-      {/* Mobile product list Mouses*/}
+      {/* Mobile product list 02 - Mouses*/}
       <section className="my-5 md:hidden">
         <h2 className="mb-2 pl-5 text-lg font-semibold">Mouses</h2>
         <ProductList products={mouses} />
       </section>
+
+      {/* Mobile banner 03 - Speakers */}
+      <div className="md:hidden">
+        <div className="my-5 flex w-full gap-2">
+          <Image
+            src="/banner-speakers.png"
+            alt="Até 15% de desconto em caixas de som"
+            width={0}
+            height={0}
+            className="h-auto w-full object-cover"
+            sizes="100vw"
+            priority
+          />
+        </div>
+      </div>
+
+      {/* Mobile product list 03 - Speakers*/}
+      <section className="my-5 md:hidden">
+        <h2 className="mb-2 pl-5 text-lg font-semibold">Caixas de som</h2>
+        <ProductList products={speakers} />
+      </section>
+
+      {/* Mobile banner 04 - Monitors */}
+      <div className="md:hidden">
+        <div className="my-5 flex w-full gap-2">
+          <Image
+            src="/banner-monitors.png"
+            alt="Até 15% de desconto em monitores"
+            width={0}
+            height={0}
+            className="h-auto w-full object-cover"
+            sizes="100vw"
+            priority
+          />
+        </div>
+      </div>
+
+      {/* Mobile product list 04 - Monitors*/}
+      <section className="my-5 md:hidden">
+        <h2 className="mb-2 pl-5 text-lg font-semibold">Monitores</h2>
+        <ProductList products={monitors} />
+      </section>
+
+      {/* Mobile banner 05 - Delivery */}
+      <div className="md:hidden">
+        <div className="mt-5 flex w-full gap-2">
+          <Image
+            src="/banner-fretemobile.png"
+            alt="Frete grátis para todo o Brasil"
+            width={0}
+            height={0}
+            className="h-auto w-full object-cover"
+            sizes="100vw"
+            priority
+          />
+        </div>
+      </div>
     </main>
   );
 }
