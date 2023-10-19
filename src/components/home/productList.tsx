5c45085c2cb2893d1bff81185a7539b91eeeb63e
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-19 18:13:23 -0300

    feat: add mouse wheel lateral scroll to product list

--- src/components/home/productList.tsx
+++ src/components/home/productList.tsx
@@ -1,20 +1,47 @@
+"use client";
 import { Product } from "@prisma/client";
 import ProductItem from "../common/productItem";
 import { computeProductTotalPrice } from "@/helpers/product";
+import { useRef } from "react";
 
 interface ProductListProps {
   products: Product[];
 }
 
 const ProductList = ({ products }: ProductListProps) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleWheelScroll = (e: React.WheelEvent<HTMLDivElement>) => {
+    if (containerRef.current) {
+      const scrollAmount = e.deltaY;
+      containerRef.current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const handleHover = () => {
+    window.addEventListener("wheel", preventScroll, { passive: false });
+  };
+
+  const handleHoverExit = () => {
+    window.removeEventListener("wheel", preventScroll);
+  };
+
+  const preventScroll = (e: WheelEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    //  scrollbar-thin scrollbar-thumb-[#22222a] scrollbar-track-white
-    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+    <div
+      className="md:scrollbar-thin scrollbar-thumb-[#727272] dark:scrollbar-thumb-[#22222a] scrollbar-track-white flex w-full gap-4 overflow-x-auto px-5"
+      onWheel={handleWheelScroll}
+      ref={containerRef}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHoverExit}
+    >
       {products.map((product) => (
-        <ProductItem
-          product={computeProductTotalPrice(product)}
-          key={product.id}
-        />
+        <div key={product.id} className="mb-8">
+          <ProductItem product={computeProductTotalPrice(product)} />
+        </div>
       ))}
     </div>
   );
