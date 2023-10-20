d0c0660aa0c9ce1186905485e8b59125a07c7f1c
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-20 18:52:17 -0300

    feat: add category icon constant

---
+++ src/constants/categoryIcon.tsx
@@ -0,0 +1,12 @@
+import { BsKeyboard, BsMouse3, BsHeadset, BsSpeaker } from "react-icons/bs";
+import { FiMonitor } from "react-icons/fi";
+import { SquareIcon } from "lucide-react";
+
+export const CATEGORY_ICON = {
+  keyboards: <BsKeyboard size={20} />,
+  monitors: <FiMonitor size={20} />,
+  headphones: <BsHeadset size={20} />,
+  mousepads: <SquareIcon size={20} />,
+  speakers: <BsSpeaker size={20} />,
+  mouses: <BsMouse3 size={20} />,
+};
