8eb91df079e1f331237062e1c2af3522ddb38e2f
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-20 17:45:28 -0300

    style: add gamtech blue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        gamtech: "#1267dc",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
