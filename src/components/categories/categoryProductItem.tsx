87cb1de1546f42d8b67b0cee39ede5daae620800
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-20 18:52:39 -0300

    feat: add Category Product Item component

---
+++ src/components/categories/categoryProductItem.tsx
@@ -0,0 +1,72 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { Chip } from "@nextui-org/react";
+import Image from "next/image";
+import { BsArrowDownShort } from "react-icons/bs";
+
+interface ProductItemProps {
+  product: ProductWithTotalPrice;
+}
+
+const CategoryProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="flex flex-col gap-4">
+      {/* Image */}
+      <div className="relative flex h-40 w-full items-center justify-center rounded-lg bg-white/80 dark:bg-neutral-800">
+        <Image
+          src={product.imageUrls[0]}
+          alt={product.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-24 w-auto max-w-[90%]"
+          style={{ objectFit: "contain" }}
+        />
+        {product.discountPercentage > 0 && (
+          <Chip
+            className="absolute left-2 top-2"
+            variant="solid"
+            size="sm"
+            startContent={<BsArrowDownShort size={20} />}
+          >
+            <p className="text-tiny">{product.discountPercentage}%</p>
+          </Chip>
+        )}
+      </div>
+
+      {/* Text */}
+      <div className="flex flex-col gap-1 text-center">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-tiny">
+          {product.name}
+        </p>
+
+        <div className="flex items-center gap-2 justify-center">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="text-xs font-extrabold text-[#1267dc]">
+                {product.totalPrice.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
+              <p className="text-xs line-through opacity-75">
+                {Number(product.basePrice).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
+            </>
+          ) : (
+            <p className="text-xs font-extrabold text-[#1267dc]">
+              {Number(product.basePrice).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryProductItem;
