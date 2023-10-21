7a6dd40dd4cd3e92f39e50a61de428c9d35d5f4b
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-21 16:28:03 -0300

    feat: add supabase domain to nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fsw-store.s3.sa-east-1.amazonaws.com"],
+    domains: [
+      "fsw-store.s3.sa-east-1.amazonaws.com",
+      "zxsxlbutylcszksglhqh.supabase.co",
+    ],
   },
 };
 
