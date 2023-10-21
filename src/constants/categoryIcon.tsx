56b96b152e18ba903d4808ad234b6cb9ae03ef96
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-21 16:28:28 -0300

    feat: add more categoryicons

--- src/constants/categoryIcon.tsx
+++ src/constants/categoryIcon.tsx
@@ -1,6 +1,8 @@
-import { BsKeyboard, BsMouse3, BsHeadset, BsSpeaker } from "react-icons/bs";
+import { BsKeyboard, BsMouse3, BsHeadset, BsSpeaker, BsGpuCard } from "react-icons/bs";
 import { FiMonitor } from "react-icons/fi";
 import { SquareIcon } from "lucide-react";
+import { GiGameConsole } from 'react-icons/gi'
+import { PiComputerTowerBold } from 'react-icons/pi'
 
 export const CATEGORY_ICON = {
   keyboards: <BsKeyboard size={20} />,
@@ -9,4 +11,7 @@ export const CATEGORY_ICON = {
   mousepads: <SquareIcon size={20} />,
   speakers: <BsSpeaker size={20} />,
   mouses: <BsMouse3 size={20} />,
+  consoles: <GiGameConsole size={20} />,
+  gpu: <BsGpuCard size={20} />,
+  cases: <PiComputerTowerBold size={20}/>,
 };
