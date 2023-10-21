e22bfcf258228e6dc354cb116495c31c7f42e5f1
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-21 16:29:10 -0300

    feat: add more product lists e banners to mobile UI

--- src/components/home/mobileHomeUI.tsx
+++ src/components/home/mobileHomeUI.tsx
@@ -4,6 +4,7 @@ import { ProductListSection } from "./productListSection";
 import Image from "next/image";
 import ProductList from "./productList";
 import CategoryList from "./categoryList";
+import { BANNER_LINK } from "@/constants/bannerLink";
 
 export default async function MobileUI() {
   const categories = await prismaClient.category.findMany({});
@@ -42,12 +43,15 @@ export default async function MobileUI() {
   const speakers = await fetchProductsByCategory("speakers");
   const keyboards = await fetchProductsByCategory("keyboards");
   const mousepads = await fetchProductsByCategory("mousepads");
+  const cases = await fetchProductsByCategory("cases");
+  const gpu = await fetchProductsByCategory("gpu");
+  const consoles = await fetchProductsByCategory("consoles");
 
   return (
     <main>
       <div className="md:hidden">
         <Image
-          src="/banner-deals-mobile.png"
+          src={BANNER_LINK.banner_deals_mobile}
           alt="Até 35% de desconto só esse mês"
           width={0}
           height={0}
@@ -69,45 +73,63 @@ export default async function MobileUI() {
       </div>
 
       <BannerSection
-        imageSrc="/banner-headphones.png"
-        altText="Até 20% de desconto em headphones"
-      />
-      <ProductListSection title="Headphones" products={headphones} />
-
-      <BannerSection
-        imageSrc="/banner-mouses.png"
+        imageSrc={BANNER_LINK.banner_mouses}
         altText="Até 15% de desconto em mouses"
       />
       <ProductListSection title="Mouses" products={mouses} />
 
       <BannerSection
-        imageSrc="/banner-speakers.png"
-        altText="Até 15% de desconto em caixas de som"
+        imageSrc={BANNER_LINK.banner_gpu}
+        altText="Até 30% de desconto em placas de vídeo"
       />
-      <ProductListSection title="Caixas de som" products={speakers} />
+      <ProductListSection title="Placas de vídeo" products={gpu} />
 
       <BannerSection
-        imageSrc="/banner-monitors.png"
+        imageSrc={BANNER_LINK.banner_keyboards}
+        altText="Até 25% de desconto em teclados"
+      />
+      <ProductListSection title="Teclados" products={keyboards} />
+
+      <BannerSection
+        imageSrc={BANNER_LINK.banner_monitors}
         altText="Até 15% de desconto em monitores"
       />
       <ProductListSection title="Monitores" products={monitors} />
 
       <BannerSection
-        imageSrc="/banner-mousepads.png"
+        imageSrc={BANNER_LINK.banner_case}
+        altText="Até 20% de desconto em gabinetes"
+      />
+      <ProductListSection title="Gabinetes" products={cases} />
+
+      <BannerSection
+        imageSrc={BANNER_LINK.banner_speakers}
+        altText="Até 15% de desconto em caixas de som"
+      />
+      <ProductListSection title="Caixas de som" products={speakers} />
+
+      <BannerSection
+        imageSrc={BANNER_LINK.banner_mousepads}
         altText="Até 15% de desconto em mousepads"
       />
       <ProductListSection title="Mousepads" products={mousepads} />
 
       <BannerSection
-        imageSrc="/banner-keyboards.png"
-        altText="Até 25% de desconto em teclados"
+        imageSrc={BANNER_LINK.banner_headphones}
+        altText="Até 20% de desconto em headphones"
       />
-      <ProductListSection title="Teclados" products={keyboards} />
+      <ProductListSection title="Headphones" products={headphones} />
+
+      <BannerSection
+        imageSrc={BANNER_LINK.banner_consoles}
+        altText="Até 10% de desconto em consoles"
+      />
+      <ProductListSection title="Consoles" products={consoles} />
 
       <div className="md:hidden">
         <div className="mt-5 flex w-full gap-2">
           <Image
-            src="/banner-fretemobile.png"
+            src={BANNER_LINK.banner_fretemobile}
             alt="Frete grátis para todo o Brasil"
             width={0}
             height={0}
