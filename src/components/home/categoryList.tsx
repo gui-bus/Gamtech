5bbeab85ef245a36720311025a5a6d457332fa97
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-22 18:23:44 -0300

    style: remove styles and change size

--- src/components/home/categoryList.tsx
+++ src/components/home/categoryList.tsx
@@ -10,7 +10,7 @@ const CategoryList = ({ category }: CategoryItemProps) => {
   return (
     <Link href={`/category/${category.slug}`}>
       <Button
-        className="flex w-36 cursor-pointer items-center justify-center gap-2 rounded-md bg-white p-2 drop-shadow-lg transition-all hover:bg-zinc-300 dark:bg-foreground dark:text-black hover:dark:bg-foreground-900/90 "
+        className="flex w-40"
         startContent={
           CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]
         }
