a2689dddb8810d3dcf7b4873f3f581051a4962ce
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-22 18:23:25 -0300

    feat: add gamepad & cpu icons

--- src/constants/categoryIcon.tsx
+++ src/constants/categoryIcon.tsx
@@ -1,8 +1,9 @@
-import { BsKeyboard, BsMouse3, BsHeadset, BsSpeaker, BsGpuCard } from "react-icons/bs";
+import { BsKeyboard, BsMouse3, BsHeadset, BsSpeaker, BsGpuCard, BsCpu } from "react-icons/bs";
 import { FiMonitor } from "react-icons/fi";
 import { SquareIcon } from "lucide-react";
 import { GiGameConsole } from 'react-icons/gi'
 import { PiComputerTowerBold } from 'react-icons/pi'
+import { LiaGamepadSolid } from 'react-icons/lia'
 
 export const CATEGORY_ICON = {
   keyboards: <BsKeyboard size={20} />,
@@ -14,4 +15,6 @@ export const CATEGORY_ICON = {
   consoles: <GiGameConsole size={20} />,
   gpu: <BsGpuCard size={20} />,
   cases: <PiComputerTowerBold size={20}/>,
+  gamepads: <LiaGamepadSolid size={20}/>,
+  cpu: <BsCpu size={20}/>,
 };
