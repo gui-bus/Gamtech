91ecc002019608ae732d49e3b9b1fbb526606f62
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-23 17:32:27 -0300

    feat: add product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,11 @@
+import CategoryProductItem from "@/components/categories/categoryProductItem";
+import ProductList from "@/components/home/productList";
 import ProductImages from "@/components/productDetails/productImages";
+import ProductInfo from "@/components/productDetails/productInfo";
+import { Separator } from "@/components/ui/separator";
+import { computeProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
-import React from "react";
+import React, { useEffect } from "react";
 
 interface ProductDetailsProps {
   params: {
@@ -13,18 +18,50 @@ const ProductDetails = async ({ params: { slug } }: ProductDetailsProps) => {
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!product) return null;
 
   return (
     <div className="pb-5">
-      <div className="pb-2">
-        <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <div className="flex w-full flex-col pb-2 lg:flex-row">
+        <div className="w-full">
+          <ProductImages product={product} />
+        </div>
+
+        <div className="w-full">
+          <ProductInfo product={computeProductTotalPrice(product)} />
+        </div>
       </div>
 
-      <div className="px-5">
-        <h1 className="text-sm font-medium text-center">{product?.name}</h1>
+      <Separator className="mx-auto my-4 w-full max-w-[95vw]" />
+
+      <div className="lg:hidden">
+        <ProductList products={product.category.products} />
+      </div>
+
+      <div className="hidden lg:block">
+        <div className="grid grid-cols-2 gap-4 px-5 pb-8 pt-3 md:grid-cols-5">
+          {product.category.products.map((product) => (
+            <CategoryProductItem
+              key={product.id}
+              product={computeProductTotalPrice(product)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/productDetails/productInfo.tsx
@@ -0,0 +1,140 @@
+"use client";
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { Accordion, AccordionItem, Button, Chip } from "@nextui-org/react";
+import React, { useState } from "react";
+import { BsArrowDownShort } from "react-icons/bs";
+import { BiSolidChevronRight, BiSolidChevronLeft } from "react-icons/bi";
+import { HiShoppingCart } from "react-icons/hi";
+import { FaTruckFast } from "react-icons/fa6";
+import { Separator } from "../ui/separator";
+
+interface ProductInfoProps {
+  product: Pick<
+    ProductWithTotalPrice,
+    "basePrice" | "description" | "discountPercentage" | "name" | "totalPrice"
+  >;
+}
+
+const ProductInfo = ({
+  product: { basePrice, description, discountPercentage, name, totalPrice },
+}: ProductInfoProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDecreaseQuantity = () => {
+    setQuantity((prev) => (prev === 1 ? prev : prev - 1));
+  };
+
+  const handleIncreaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  return (
+    <div className="flex flex-col bg-white/50  p-5 dark:bg-neutral-800 lg:rounded-bl-3xl">
+      <h2 className="text-center text-lg">{name}</h2>
+
+      <div className="my-2 flex items-center justify-center gap-2">
+        {discountPercentage > 0 ? (
+          <>
+            <Chip
+              variant="solid"
+              size="sm"
+              startContent={<BsArrowDownShort size={20} />}
+            >
+              <p className="text-tiny">{discountPercentage}%</p>
+            </Chip>
+            <p className="text-xl font-extrabold text-[#1267dc]">
+              {totalPrice.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+            <p className="text-sm line-through opacity-75">
+              {Number(basePrice).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+          </>
+        ) : (
+          <p className="text-xl font-extrabold text-[#1267dc]">
+            {Number(basePrice).toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </p>
+        )}
+      </div>
+
+      <div className="my-4 flex w-full flex-col items-center justify-center gap-4">
+        <div className="flex items-center justify-center">
+          <Button
+            isIconOnly
+            variant="shadow"
+            color="primary"
+            startContent={<BiSolidChevronLeft size={20} />}
+            size="sm"
+            onClick={handleDecreaseQuantity}
+          />
+          <span className="px-4">{quantity}</span>
+          <Button
+            isIconOnly
+            variant="shadow"
+            color="primary"
+            startContent={<BiSolidChevronRight size={20} />}
+            size="sm"
+            onClick={handleIncreaseQuantity}
+          />
+        </div>
+
+        <div className="w-full max-w-xl">
+          <Button
+            endContent={<HiShoppingCart size={20} />}
+            variant="shadow"
+            color="primary"
+            className="w-full font-bold"
+          >
+            Adicionar ao carrinho
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between rounded-lg bg-white/50 px-5 py-3 text-xs dark:bg-neutral-900 md:text-sm">
+        <div className="flex items-center justify-center gap-2">
+          <FaTruckFast size={40} />
+          <div>
+            <p>
+              Entrega via <span className="font-bold">Gampack &reg;</span>
+            </p>
+            <p className="font-bold text-primary">
+              Envio para <span className="font-bold">todo o Brasil</span>
+            </p>
+          </div>
+        </div>
+
+        <p className="font-bold">Frete Grátis</p>
+      </div>
+
+
+      <div className="mt-8 flex flex-col gap-3">
+        <Accordion variant="shadow" className="bg-white/50 dark:bg-neutral-900">
+          <AccordionItem
+            key="1"
+            aria-label="Sobre o produto"
+            title={<h1 className="font-semibold ml-2">Sobre o produto</h1>}
+            subtitle={
+              <p className="text-tiny opacity-80 ml-2">
+                Clique para ver mais detalhes
+              </p>
+            }
+          >
+            <p className="px-2 pb-4 text-justify opacity-80">
+              {description.replace(/\\n/g, "\n")}
+            </p>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default ProductInfo;
