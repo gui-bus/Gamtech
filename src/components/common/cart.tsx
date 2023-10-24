965badfdd422d812c0070141088fd0d28f246bce
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-24 15:15:41 -0300

    style: add max height to cart scroll

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -13,7 +13,7 @@ const Cart = () => {
     <div className="relative">
       <div className="my-5 flex flex-col gap-2 overflow-hidden">
         <div
-          className={`h-80 ${
+          className={`h-full max-h-96 md:max-h-80 ${
             products.length > 0 && "overflow-y-scroll"
           } px-2 scrollbar-track-white scrollbar-thumb-[#727272] dark:scrollbar-thumb-[#22222a] md:scrollbar-thin`}
         >
