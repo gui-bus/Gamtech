af1efd8fbb60a6dea9e1b06e65c04f6fb6594a6d
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-24 14:09:57 -0300

    feat: show total, subtotal and total discount on cart

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -3,30 +3,82 @@ import React, { useContext } from "react";
 import CartItem from "../cart/cartItem";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Button, Link } from "@nextui-org/react";
-import { TbCategory2 } from 'react-icons/tb'
+import { TbCategory2 } from "react-icons/tb";
+import { Separator } from "../ui/separator";
 
 const Cart = () => {
-  const { products } = useContext(CartContext);
+  const { products, subtotal, total, totalDiscount } = useContext(CartContext);
   return (
-    <div className="my-5 flex flex-col gap-2 overflow-y-auto">
-      {products.length > 0 ? (
-        products.map((product) => (
-          <CartItem
-            key={product.id}
-            product={computeProductTotalPrice(product as any) as any}
-          />
-        ))
-      ) : (
-        <div className="flex flex-col items-center justify-center text-center my-5">
-          <h4 className="font-semibold">Você ainda não possui nenhum produto no carrinho!</h4>
-          <p>Que tal dar uma olhada nas opções?</p>
+    <>
+      <div className="my-5 flex flex-col gap-2 overflow-y-auto">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <CartItem
+              key={product.id}
+              product={computeProductTotalPrice(product as any) as any}
+            />
+          ))
+        ) : (
+          <div className="my-5 flex flex-col items-center justify-center text-center">
+            <h4 className="font-semibold">
+              Você ainda não possui nenhum produto no carrinho!
+            </h4>
+            <p>Que tal dar uma olhada nas opções?</p>
 
-          <Link href="/categories" className="mt-4 w-full mx-auto">
-            <Button endContent={<TbCategory2 size={20}/>} className="w-full font-semibold" variant="shadow" color="primary">Ver catálogo de produtos</Button>
-          </Link>
+            <Link href="/categories" className="mx-auto mt-4 w-full">
+              <Button
+                endContent={<TbCategory2 size={20} />}
+                className="w-full font-semibold"
+                variant="shadow"
+                color="primary"
+              >
+                Ver catálogo de produtos
+              </Button>
+            </Link>
+          </div>
+        )}
+      </div>
+
+      {products.length > 0 && (
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between text-sm">
+            <p className="font-semibold">Subtotal</p>
+            <p>
+              {subtotal.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between text-sm">
+            <p className="font-semibold">Descontos</p>
+            <p className="line-through opacity-80">
+              {totalDiscount.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between text-sm">
+            <p className="font-semibold">Frete</p>
+            <p className="uppercase">Grátis</p>
+          </div>
+
+          <Separator />
+          <div className="flex items-center justify-between text-base">
+            <p className="font-semibold">Total</p>
+            <p className="font-bold text-gamtech">
+              {total.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
