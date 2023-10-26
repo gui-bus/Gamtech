953a15ee7e506d96ec0fb397e2c9492504475b9d
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-26 16:42:16 -0300

    feat: ensure order is updated on checkout success

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -3,7 +3,7 @@
 import { CartProduct } from "@/providers/cart";
 import Stripe from "stripe";
 
-export const createCheckout = async (products: CartProduct[]) => {
+export const createCheckout = async (products: CartProduct[], orderId: string) => {
   // ?Create checkout
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2023-10-16",
@@ -14,6 +14,9 @@ export const createCheckout = async (products: CartProduct[]) => {
     mode: "payment",
     success_url: "https://gamtech.vercel.app/",
     cancel_url: "https://gamtech.vercel.app/",
+    metadata: {
+      orderId,
+    },
     line_items: products.map((product) => {
       return {
         price_data: {
