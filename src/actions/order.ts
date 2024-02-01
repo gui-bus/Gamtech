57e026a45df80390a1c2ab63822ff36820fb5f0d
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-01 18:42:59 -0300

    fix: fix db error

--- src/actions/order.ts
+++ src/actions/order.ts
@@ -2,6 +2,8 @@
 import { prismaClient } from "@/lib/prisma";
 import { CartProduct } from "@/providers/cart";
 
+
+
 export const createOrder = async (cartProducts: CartProduct[], userId: string) => {
   const order = await prismaClient.order.create({
     data: {
